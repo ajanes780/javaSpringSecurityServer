1fbf9cc2b5de1e6999fb0c20d0c67d5f4537a95d
Aaron Janes <aaron.janes@gmail.com>
2021-10-03 15:51:09 -0600

    Set up basic server with auth

---
+++ src/main/java/com/ignite/aaronpractice/AaronpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.ignite.aaronpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaronpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaronpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ignite/aaronpractice/AaronpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ignite.aaronpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaronpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
